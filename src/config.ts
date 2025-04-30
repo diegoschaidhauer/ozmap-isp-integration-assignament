ea6a2e8f2de88b624b18fddd85b68174882a7d4f
diegoschaidhauer <xaxim84@gmail.com>
2025-04-30 17:09:11 -0300

    database e atualizacao do config

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export const config = {
   ispApiUrl: process.env.ISP_API_URL || 'http://localhost:4000',
   ozmapApiKey: process.env.OZMAP_API_KEY || '',
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/ozmap_sync',
 };
